6ed35a695c137441d563f976cdb4ca05dfacf729
Z3vron <92884075+Z3vron@users.noreply.github.com>
2022-11-10 15:49:18 +0100

    Update Player_attack.cs

--- Game_newest_version/Assets/Scripts/Player/Player_attack.cs
+++ Game_newest_version/Assets/Scripts/Player/Player_attack.cs
@@ -2,35 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-
-
 namespace Attack{
     public class Player_attack : MonoBehaviour{
-        
-        public float damage_given_left = 0;
-        public float damage_given_right = 0;
-        public float attack_durability_cost = 0;
-        
-        [SerializeField] private Rigidbody _projectile_prefab_model;
-        [SerializeField] private Transform _root_for_shooting_projectile;
-        [SerializeField] private float _projectile_speed = 2000f;
-        private float _player_strength;
-        private float _player_stamina;
-        private bool _Is_in_air;
-        private bool _Is_crouching;
-        private int _last_light_attack = 0;
-        private Player_statistics _player_stats;
-        private Player_inventory_info.Player_inventory _player_inventory;
         public Animator animator;
-        //can create less 
-        [SerializeField] private List<Player_attacks> _player_list_light_attacks;
-        [SerializeField] private List<Player_attacks> _player_list_heavy_attacks;
+        #region Info for weapon_damage_collider
+            [Header("Info for weapon_damage_collider")]
+            public float damage_given_left = 0;
+            public float damage_given_right = 0;
+            public float attack_durability_cost = 0;
+        #endregion
+        #region Variables about shooting projectile
+            [Header("Variables about shooting projectile")]
+            [SerializeField] private Rigidbody _projectile_prefab_model;
+            [SerializeField] private Transform _root_for_shooting_projectile;
+            [SerializeField] private float _projectile_speed = 2000f;
+        #endregion
+        #region Player stats/info
+            private float _player_strength;
+            private float _player_stamina;
+            private bool _Is_in_air;
+            private bool _Is_crouching;
+            private Player_statistics _player_stats;
+        #endregion
+        private bool _last_light_attack_right = false;
+        private bool _last_heavy_attack_right = false;
+        private Player_inventory_info.Player_inventory _player_inventory;
+        #region  Lists of possible player attacks(used for playing given animation)
+            [Header("Lists of possible player attacks")]
+            [SerializeField] private List<Player_attacks> _player_list_light_attacks;
+            [SerializeField] private List<Player_attacks> _player_list_heavy_attacks;
+            [SerializeField] private List<Player_attacks> _player_list_combo_attacks;
+            [SerializeField] private List<Player_attacks> _player_list_air_special_attacks;
+        #endregion
         // !! Before Instantiate cant get to the data but after it i can just use get component as example below, cant assing objects to the overall prefab but to the instantiate in ine scene yes.
         // change flags that scripts get from prefabs to script on pfebbas to get values by themselves - think about it - probably change quite few things - especially arming player  with weapons - pivots left and right lots of 
         // unnecessary code and values etc
         private void Start() {
-            animator = gameObject.GetComponentInChildren<Animator>();
+            animator = GetComponentInChildren<Animator>();
             _player_stats = GetComponent<Player_info>().player_stats;
             _player_inventory = GetComponent<Player_inventory_info.Player_inventory>();
         }
@@ -74,8 +82,9 @@ namespace Attack{
         public void Handle_light_attack(){
             attack_durability_cost = 1;
             if(! _Is_in_air && !_Is_crouching){      
-                //Debug.Log("Light ground attack should perform");
                 //no weapons in either hand
+                if(_player_inventory.current_weapon_for_left_hand == _player_inventory.unarmed && _player_inventory.current_weapon_for_right_hand == _player_inventory.unarmed)
+                    Debug.Log("Fist light attack here");
                 //weapon in only right hand
                 if(_player_inventory.current_weapon_for_right_hand != _player_inventory.unarmed && _player_inventory.current_weapon_for_left_hand == _player_inventory.unarmed){
                     Start_right_light_attack();
@@ -86,16 +95,15 @@ namespace Attack{
                 }
                 // weapons in both hands
                 else if(_player_inventory.current_weapon_for_left_hand != _player_inventory.unarmed && _player_inventory.current_weapon_for_right_hand != _player_inventory.unarmed){
-                    if(_last_light_attack == 0){
+                    if(_last_light_attack_right){
                         Start_left_light_attack();
-                        _last_light_attack =1;
+                        _last_light_attack_right= false;
                     }  
-                    else if (_last_light_attack == 1){
+                    else if (!_last_light_attack_right){
                         Start_right_light_attack();
-                        _last_light_attack = 0;
+                        _last_light_attack_right = true;
                     }
                 }
-                //_animator.SetFloat("Attack_speed_multiplayer",1);   
             }
             else if (_Is_in_air){
                 //animator.CrossFadeInFixedTime("Override.Air_attack_01",0f,1);
@@ -103,15 +111,27 @@ namespace Attack{
             }
         }
         public void Handle_heavy_attack(){
-            attack_durability_cost = 2;
-            if(_player_inventory.current_weapon_for_right_hand != _player_inventory.unarmed && _player_inventory.current_weapon_for_left_hand == _player_inventory.unarmed){
-                Start_right_heavy_attack();
+            if(! _Is_in_air && !_Is_crouching){      
+                attack_durability_cost = 2;
+                if(_player_inventory.current_weapon_for_left_hand == _player_inventory.unarmed && _player_inventory.current_weapon_for_right_hand == _player_inventory.unarmed)
+                        Debug.Log("Fist heavy attack here");
+                if(_player_inventory.current_weapon_for_right_hand != _player_inventory.unarmed && _player_inventory.current_weapon_for_left_hand == _player_inventory.unarmed){
+                    Start_right_heavy_attack();
+                }
+                else if(_player_inventory.current_weapon_for_left_hand != _player_inventory.unarmed && _player_inventory.current_weapon_for_right_hand == _player_inventory.unarmed){
+                    Start_left_heavy_attack();
+                }
+                else if(_player_inventory.current_weapon_for_left_hand != _player_inventory.unarmed && _player_inventory.current_weapon_for_right_hand != _player_inventory.unarmed){
+                    if(_last_heavy_attack_right){
+                        Start_left_heavy_attack();
+                        _last_heavy_attack_right = false;
+                    }  
+                    else if (!_last_heavy_attack_right){
+                        Start_right_heavy_attack();
+                        _last_heavy_attack_right = true;
+                    }
+                }
             }
-            else if(_player_inventory.current_weapon_for_left_hand != _player_inventory.unarmed && _player_inventory.current_weapon_for_right_hand == _player_inventory.unarmed){
-                Start_left_heavy_attack();
-            }
-                
-            //Debug.Log("Strong ground attack was performed");
         }
         public void Handle_special_attack(){
             //    Debug.Log("Special attack was performed");
@@ -196,6 +216,9 @@ namespace Attack{
             _player_stats.Take_stamina(_player_inventory.current_weapon_for_right_hand.light_attack_stamina_cost);
         }
         public void Start_right_heavy_attack(){
+            if(_player_stamina < _player_inventory.current_weapon_for_right_hand.strong_attack_stamina_cost)
+                return;
+            damage_given_right = _player_inventory.current_weapon_for_right_hand.Strong_attack_damage + _player_strength;
             if(_player_inventory.current_weapon_for_right_hand is Dagger){
                 Dagger dagger = (Dagger)_player_inventory.current_weapon_for_right_hand;
                 animator.SetFloat("Attack_speed_multiplayer",dagger.attack_speed_multiplayer);
@@ -207,10 +230,19 @@ namespace Attack{
             else{
                 animator.SetFloat("Attack_speed_multiplayer",1);
             }
-            animator.CrossFadeInFixedTime("Override.Strong_attack_right",0f,1);
+            int attack_num;
+            while(true){
+                attack_num = Random.Range(0,_player_list_heavy_attacks.Count);
+                if(_player_list_heavy_attacks[attack_num].require_right_weapon)
+                    break;
+            }
+            animator.CrossFadeInFixedTime(_player_list_heavy_attacks[attack_num].attack_name,0f,1);
             _player_stats.Take_stamina(_player_inventory.current_weapon_for_right_hand.strong_attack_stamina_cost);
         }
         public void Start_left_heavy_attack(){
+            if(_player_stamina < _player_inventory.current_weapon_for_left_hand.strong_attack_stamina_cost)
+                return;
+            damage_given_left = _player_inventory.current_weapon_for_left_hand.Strong_attack_damage + _player_strength;
             if(_player_inventory.current_weapon_for_left_hand is Dagger){
                 Dagger dagger = (Dagger)_player_inventory.current_weapon_for_left_hand;
                 animator.SetFloat("Attack_speed_multiplayer",dagger.attack_speed_multiplayer);
@@ -222,7 +254,13 @@ namespace Attack{
             else{
                 animator.SetFloat("Attack_speed_multiplayer",1);
             }
-            animator.CrossFadeInFixedTime("Override.Strong_attack_left",0f,1);
+            int attack_num;
+            while(true){
+                attack_num = Random.Range(0,_player_list_heavy_attacks.Count);
+                if(_player_list_heavy_attacks[attack_num].require_left_weapon)
+                    break;
+            }
+            animator.CrossFadeInFixedTime(_player_list_heavy_attacks[attack_num].attack_name,0f,1);
             _player_stats.Take_stamina(_player_inventory.current_weapon_for_left_hand.strong_attack_stamina_cost);
         }
     }
