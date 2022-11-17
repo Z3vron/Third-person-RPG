677e01334cb6cfa3d1eed2d483fb79eff2d5e7d0
Z3vron <92884075+Z3vron@users.noreply.github.com>
2022-11-17 16:04:18 +0100

    Update Player_inventory.cs

--- Game_newest_version/Assets/Scripts/Player/Player_inventory.cs
+++ Game_newest_version/Assets/Scripts/Player/Player_inventory.cs
@@ -3,56 +3,71 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Cinemachine;
-
+//ctr + k , ctr + 0(zero) collapse(fold) all functions/regions/classes etc
+//ctr + shift+[ collapse(fold) active(where is mouse course) function/region/class etc
 
 namespace Player_inventory_info{
     public class Player_inventory : MonoBehaviour{
         //for tests
-         public Item_info.Item item_to_add;
-        // public Item_info.Item material_to_add;
-        // public Item_info.Item material_to_add2;
-
-        public Weapon_info.Weapon current_weapon_for_right_hand;
-        public Weapon_info.Weapon backup_weapon_right;
-        public Weapon_info.Weapon current_weapon_for_left_hand;
-        public Weapon_info.Weapon backup_weapon_left;
-        public Weapon_info.Weapon unarmed;
-        public Potions empty_potion;
+        #region Items to add and their amounts at start to player inv(for tests)
+            [Header("Items to add and their amounts at start to player inv(for tests)")]
+            [SerializeField] private Item_info.Item _item_to_add_1;
+            [SerializeField] private int _item_to_add_1_amount;
+            [SerializeField] private Item_info.Item _item_to_add_2;
+            [SerializeField] private int _item_to_add_2_amount;
+            [SerializeField] private Item_info.Item _item_to_add_3;
+            [SerializeField] private int _item_to_add_3_amount;
+        #endregion
+        #region Weapons and potions in quick slots
+            [Header("Weapons and potions in quick slots")]       
+            public Weapon_info.Weapon current_weapon_for_right_hand;
+            public Weapon_info.Weapon backup_weapon_right;
+            public Weapon_info.Weapon current_weapon_for_left_hand;
+            public Weapon_info.Weapon backup_weapon_left;
+            public Weapon_info.Weapon unarmed;
+            public List<Slot> quick_slots_potions = new List<Slot>();
+            public Potions empty_potion;
+        #endregion
         public Armor_info.Armour player_armour;
-        public List<Weapon_info.Weapon> inventory_weapons_slots = new List<Weapon_info.Weapon>();
-        public List<Slot> quick_slots_potions = new List<Slot>();
-        public List<Potions> inventory_potions_slots = new List<Potions>();
-        public List<Materials> inventory_materials_slots = new List<Materials>();
-        public List<Item_info.Item> inventory_items_slots = new List<Item_info.Item>();
+        #region Items in player inventory divided by item category
+            [Header("Lists of items in player inventory")]
+            public List<Weapon_info.Weapon> inventory_weapons_slots = new List<Weapon_info.Weapon>();
+            public List<Potions> inventory_potions_slots = new List<Potions>();
+            public List<Materials> inventory_materials_slots = new List<Materials>();
+            public List<Item_info.Item> inventory_items_slots = new List<Item_info.Item>();
+        #endregion
+        [Tooltip("Holds information if player inventory or crafting inventory  is open")]
         public bool inventory_open = false;
+        [Tooltip("Amount of items in 1 row in player inventory applies to all rows")]
         public int amount_of_items_slots = 6;
-
-        [SerializeField] private Inventories _inventories;
-        [SerializeField] private CinemachineFreeLook _cinemachine_camera;
-        private Input_handler _input_handler;
-        private Animator _animator;
-        private Weapon_slot_manager.Weapon_manager weapon_slot_manager;
+        #region Other scripts and components references
+            [Header("Other scripts and components references")]
+            [SerializeField] private Inventories _inventories;
+            [SerializeField] private CinemachineFreeLook _cinemachine_camera;
+            private Input_handler _input_handler;
+            private Weapon_slot_manager.Weapon_manager weapon_slot_manager;
+            private Animator _animator;
+        #endregion
+        #region Variables used to store camera values to restore them after freezing camera for browsing inventory
         private float _cinemachine_camera_x_sensitivity;
         private float _cinemachine_camera_y_sensitivity;
-        private float _poison_timer_left = 0;
-        private float _poison_timer_right = 0;
-        private float _poison_time_left;
-        private float _poison_time_right;
+        #endregion
+        #region Poison weapons variables    
+            [Tooltip("Timer how long left weapon will be poison")]
+            private float _poison_timer_left_weapon = 0;
+            private float _poison_timer_right_weapon = 0;
+            private float _poison_time_left_weapon;
+            private float _poison_time_right_weapon;
+        #endregion
         private void Awake() {
             weapon_slot_manager = GetComponent<Weapon_slot_manager.Weapon_manager>();
             _animator = GetComponentInChildren<Animator>();
         }
         private void Start() {
             //just for testing
-             _inventories.Add_item_to_player_inv_last_slot(item_to_add,1);
-            // //_inventories.Add_item_to_player_inv_last_slot(item_to_add,5);
-            // _inventories.Add_item_to_player_inv_last_slot(material_to_add,4);
-            // // _inventories.Add_item_to_player_inv_last_slot(material_to_add2,3);
-            // _inventories.Add_item_to_player_inv_last_slot(material_to_add2,9);
-            // _inventories.Add_item_to_player_inv_last_slot(material_to_add2,13);
-            // _inventories.Add_item_to_player_inv_last_slot(material_to_add2,10);
-            // _inventories.Add_item_to_player_inv_last_slot(material_to_add2,10);
-            // _inventories.Add_item_to_player_inv_last_slot(material_to_add2,10);
+            _inventories.Add_item_to_player_inv_last_slot(_item_to_add_1,_item_to_add_1_amount;
+            _inventories.Add_item_to_player_inv_last_slot(_item_to_add_2,_item_to_add_2_amount);
+            _inventories.Add_item_to_player_inv_last_slot(_item_to_add_3,_item_to_add_3_amount);           
             
             //definition here elsewhere value is changed 
             //load fists at the start to left hand
@@ -84,21 +99,21 @@ namespace Player_inventory_info{
                 weapon_slot_manager.Load_weapon_to_slot(current_weapon_for_right_hand,true);
             }
             if(current_weapon_for_right_hand.isPoisoned){
-                _poison_timer_right += Time.deltaTime;
-                if(_poison_timer_right >= _poison_time_right){
+                _poison_timer_right_weapon += Time.deltaTime;
+                if(_poison_timer_right_weapon >= _poison_time_right_weapon){
                     current_weapon_for_right_hand.isPoisoned = false;
                     current_weapon_for_right_hand.poison_damage = 0;
                     current_weapon_for_right_hand.poison_duration = 0;
-                    _poison_timer_right = 0;
+                    _poison_timer_right_weapon = 0;
                 }
             }
             if(current_weapon_for_left_hand.isPoisoned){
-                _poison_timer_left += Time.deltaTime;
-                if(_poison_timer_left >= _poison_time_left){
+                _poison_timer_left_weapon += Time.deltaTime;
+                if(_poison_timer_left_weapon >= _poison_time_left_weapon){
                     current_weapon_for_left_hand.isPoisoned = false;
                     current_weapon_for_left_hand.poison_damage = 0;
                     current_weapon_for_left_hand.poison_duration = 0;
-                    _poison_timer_left = 0;
+                    _poison_timer_left_weapon = 0;
                 }
             }
         }
@@ -397,19 +412,19 @@ namespace Player_inventory_info{
         public void Poison_weapon(Poison_potion poison_potion,bool right_weapon){
             if(right_weapon){
                 if( current_weapon_for_right_hand.isPoisoned)
-                    _poison_timer_right = 0;
+                    _poison_timer_right_weapon = 0;
                 current_weapon_for_right_hand.isPoisoned = true;
                 current_weapon_for_right_hand.poison_damage = poison_potion.poison_damage;
                 current_weapon_for_right_hand.poison_duration = poison_potion.poison_duration;
-                _poison_time_right = poison_potion.efect_duration;
+                _poison_time_right_weapon = poison_potion.efect_duration;
             }
             else{
                 if( current_weapon_for_left_hand.isPoisoned)
-                    _poison_timer_left = 0;
+                    _poison_timer_left_weapon = 0;
                 current_weapon_for_left_hand.isPoisoned = true;
                 current_weapon_for_left_hand.poison_damage = poison_potion.poison_damage;
                 current_weapon_for_left_hand.poison_duration = poison_potion.poison_duration;
-                _poison_time_left = poison_potion.efect_duration;
+                _poison_time_left_weapon = poison_potion.efect_duration;
             }
         }
         public void Handle_inventory(){
